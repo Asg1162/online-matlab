cf9595cd2f420bfbad0f618d5c267dbb37595ef8
cnwuxing <cnwuxing@43d2037e-e363-11de-8ee0-a52971d33838>
2010-01-20 22:26:35 +0000

    loadbalancer dispatches request to different servers

--- trunk/LoadBalancer/LbServer.py
+++ trunk/LoadBalancer/LbServer.py
@@ -20,11 +20,11 @@ class BackendI(Matcloud.Backend):
                 
     def addNode(self, newnode, current=None):
         print "add node %s" % newnode
-        self._hosts.append(newnode)
+        self._hosts[newnode] = 0
     
     def delNode(self, obsnode, current=None):
         # TODO can't remove brutally need to check if it's idle
-        self._hosts.remove(obsnode)
+        del self._hosts.remove[obsnode]
 
 # BackendI servant class ...
 class QueryI(Matcloud.Query):
@@ -49,50 +49,138 @@ class QueryI(Matcloud.Query):
 # BackendI servant class ...
 class FrontendI(Matcloud.Frontend):
     # Constructor and operations here...
-    _adapter = None
-    def __init__(self, host):
-        self._hosts = host
+	_adapter = None
+	def __init__(self, host):
+		self._hosts = host
 		#self._client = {}
+		self._mutex = threading.Lock()
+		self.unmap = {}
 
-        myID = Ice.Identity();
-        myID.name = "Frontend"
-        # Add the identity to the object adapter
-        #
-        self._adapter.add(self, myID)
-        # Create a proxy for the new node and
-        # add it as a child to the parent
-        #
-        thisNode = Matcloud.FrontendPrx.uncheckedCast(self._adapter.createProxy(myID))
+		myID = Ice.Identity();
+		myID.name = "Frontend"
+		# Add the identity to the object adapter
+		#
+		self._adapter.add(self, myID)
+		# Create a proxy for the new node and
+		# add it as a child to the parent
+		#
+		thisNode = Matcloud.FrontendPrx.uncheckedCast(self._adapter.createProxy(myID))
                 
-    def FrontendAgent(self, cmd, current=None):
+	def FrontendAgent(self, cmd, current=None):
 		print "received command %s" % cmd
+		if len(self._hosts) <= 0:
+			return "No available server"
+
+		if cmd[0: cmd.find(":")] == "LOGIN":
+			return self.FrontendLogin(cmd)
+		elif cmd[0: cmd.find(":")] == "LOGOUT":
+			return self.FrontendLogout(cmd)
+		else:
+			return self.FrontendCalc(cmd)
+	
+	def FrontendCalc(self, cmd, current=None):
 		rtn = ""
 		try:
-                    ic = Ice.initialize(sys.argv)
-                    bebase = ic.stringToProxy("Parser:tcp -h os40.csc.ncsu.edu -p 10001")
-                    parser = Matcloud.ParserPrx.checkedCast(bebase)
-                    if not parser:
-                        raise RuntimeError("Invalid proxy")
-                    rtn = parser.command(cmd)
-                    print " received return string %s " % rtn
-		except:
-                    traceback.print_exc()
-                    status = 1
-                   # if ic:
-                   #     try:
-                   #         ic.destroy()
-                   #     except:
-                   #         traceback.print_exc()
-                   #         status = 1
-		return "Internal Error"
-                        
-    def FrontendLogin(self, uname, current=None):
-	#self._client[uname] = self._hosts[0]
-	#connet to backend, create user space
-        pass
+			username = cmd.split(':')[1]
+			try:
+				server = self.unmap[username]
+			except KeyError:
+				return "Please login"
 
-    def FrontendLogout(self, uname, current=None):
-        pass
+			ic = Ice.initialize(sys.argv)
+			bebase = ic.stringToProxy("Parser:tcp -h "+server[0: server.find(":")]+".csc.ncsu.edu -p 10001")
+			parser = Matcloud.ParserPrx.checkedCast(bebase)
+			if not parser:
+				raise RuntimeError("Invalid proxy")
+			rtn = parser.command(cmd)
+			print "received return string %s " % rtn
+		except:
+			traceback.print_exc()
+			status = 1
+			if ic:
+				try:
+					ic.destroy()
+				except:
+					traceback.print_exc()
+			return "Internal Error"
+
+		return rtn
+
+	def FrontendLogin(self, cmd, current=None):
+		username = ""
+		minserver = ""
+		rtn = ""
+		try:
+			username = cmd[cmd.find(":")+1 : len(cmd)]
+			load = 0
+			minload = -1
+			self._mutex.acquire()
+			for k, v in self._hosts.iteritems():
+				load = v
+				if minload == -1 or load < minload:
+					minload = load
+					minserver = k
+
+			self.unmap[username] = minserver
+			self._hosts[minserver] = minload + 1;
+			self._mutex.release()
+
+			print self.unmap[username]
+
+			ic = Ice.initialize(sys.argv)
+			bebase = ic.stringToProxy("Parser:tcp -h "+minserver[0: minserver.find(":")]+".csc.ncsu.edu -p 10001")
+			parser = Matcloud.ParserPrx.checkedCast(bebase)
+			if not parser:
+				raise RuntimeError("Invalid proxy")
+			rtn = parser.command(cmd)
+			print "Login succeeded"
+		except:
+			traceback.print_exc()
+			status = 1
+			if ic:
+				try:
+					ic.destroy()
+				except:
+					traceback.print_exc()
+			return "Internal Error"
+
+		return rtn
+
+	def FrontendLogout(self, cmd, current=None):
+		server = ""
+		rtn = ""
+		try:
+			username = cmd[cmd.find(":")+1 : len(cmd)]
+			try:
+				server = self.unmap[username]
+				del self.unmap[username]
+				self._mutex.acquire()
+				if self._hosts[server] > 0:
+					self._hosts[server] -= 1
+				else:
+					self._hosts[server] = 0
+				self._mutex.release()
+			except KeyError:
+				print "KeyError found when LOGOUT"
+
+			ic = Ice.initialize(sys.argv)
+			bebase = ic.stringToProxy("Parser:tcp -h "+server[0: server.find(":")]+".csc.ncsu.edu -p 10001")
+			parser = Matcloud.ParserPrx.checkedCast(bebase)
+			if not parser:
+				raise RuntimeError("Invalid proxy")
+			rtn = parser.command(cmd)
+			print "received return string %s " % rtn
+		except:
+			traceback.print_exc()
+			status = 1
+			if ic:
+				try:
+					ic.destroy()
+				except:
+					traceback.print_exc()
+			return "Internal Error"
+
+		return rtn
 
 
 class Server(Ice.Application):
@@ -109,7 +197,7 @@ class Server(Ice.Application):
         BackendI._adapter = adapter
         QueryI._adapter = adapter
         FrontendI._adapter = adapter
-        self._hosts = []
+        self._hosts = {}
         # create the backend
         be = BackendI(self._hosts)
         qu = QueryI(self._hosts)
